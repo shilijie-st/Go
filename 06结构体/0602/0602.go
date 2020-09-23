b94c2d42cc3fcd8cc60e35ffa08d0ce0e9c01b89
slj <331650539@qq.com>
2020-09-23 22:28:22 +0800

    Part of6.5

--- 06结构体/0602/0602.go
+++ 06结构体/0602/0602.go
@@ -4,6 +4,34 @@
 */
 package main
 
-func main() {
+import "fmt"
 
+//Cat 定义Cat结构体作为"基类"
+type Cat struct {
+	Color string
+	Name  string
+}
+
+//BlackCat 定义BlackCat作为子类
+type BlackCat struct {
+	Cat //嵌入Cat，类似于派生
+}
+
+//NewCat "构造基类"
+func NewCat(name string) *Cat {
+	return &Cat{
+		Name: name,
+	}
+}
+
+//NewBlackCat "构造子类"
+func NewBlackCat(color string) *BlackCat {
+	cat := &BlackCat{}
+	cat.Color = color
+	cat.Name = "花花"
+	return cat
+}
+
+func main() {
+	fmt.Println(NewBlackCat("colorful"))
 }
